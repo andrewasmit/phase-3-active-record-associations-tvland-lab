5297ef120cc251642b2c273466f35a7170d710c2
andrew <andrewasmit@hotmail.com>
2023-02-05 13:47:41 -0600

    commit tree 40d2c96024cfae37a09331af1c850c2ad0440607

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    @arr= []
+    all = self.actors
+    all.each do |actor|
+        @arr << "#{actor.first_name} #{actor.last_name}"
+    end
+    @arr
+  end
+
+
 end
